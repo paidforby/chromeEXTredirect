0e00ce399647c0d57e1fbd96eb8e1fb1bd07482f
gcgallo <ggallo102@gmail.com>
2016-08-01 19:02:49 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,16 +1,7 @@
-chrome.webRequest.onBeforeRequest.addListener(   // Checking if it is not redirecting to all steps.
-    function(details) {
-            return {
-                redirectUrl: "http://www.github.com"
-            };
+chrome.webRequest.onBeforeRequest.addListener(
+    
+        function(details) { return {redirectUrl: "http://www.github.com/"}; },
         
-    },
-
-    // Applies to following url patterns
-    {
-        urls: [ "*://www.facebook.com/*/"]
-    },
-
-    // In request blocking mode
-    [ "blocking" ]
-);
+        {urls: ["*://www.facebook.com/*"]},
+        
+        ["blocking"]);
